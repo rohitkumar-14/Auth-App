9cf46c05c6b83e964bdb813c6b643afcdc60e3ee
Rohit Kumar <97233950+rohitkumar-14@users.noreply.github.com>
2024-10-19 10:43:57 +0530

    Update Todo.tsx

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -445,7 +445,7 @@ export const columns: ColumnDef<Todo>[] = [
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Edit Todo</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => deleteTodo(todo.id)}>Delete Todo</DropdownMenuItem>
+            <DropdownMenuItem>Delete Todo</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       );
