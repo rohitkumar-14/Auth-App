2b27006cfa8d73ff9a5e764bc9b3fe18003cfccd
Rohit Kumar <97233950+rohitkumar-14@users.noreply.github.com>
2024-10-19 11:29:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,7 @@ const [todos, setTodos] = useState<Todo[]>([]);
           <TodoTable />
         </div>
 
-        {isModalOpen && (
+{/*         {isModalOpen && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
               <h3 className="text-xl font-bold mb-4">Add New To-Do</h3>
@@ -111,7 +111,7 @@ const [todos, setTodos] = useState<Todo[]>([]);
               </form>
             </div>
             </div>
-        )}
+        )} */}
       </SignedIn>
     </>
   );
